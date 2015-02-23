7ac7442ecdda052fb5c568825f3201c6380372df
brentdavidson1 <brentdavidson1@gmail.com>
2015-02-23 13:32:05 -0800

    Pair coded with Justin to get <input> and the check mark working

--- js/app.js
+++ js/app.js
@@ -8,17 +8,33 @@ $(document).ready(function(){
 	$(".button2").click(function(){
    		$(".menu2").slideToggle('slow');
   	});
+
+  	// begin click to complete 
+
+   $('.list-items').on('click','li', function(e) {
+      $(this).toggleClass('done'); 
+    });
+
+// end click to complete
+
+	$('.add-new-item').on('keyup', function(e){
+		if(e.keyCode === 13){
+			console.log(e);
+			addItem();
+		}
+	})
+
+    $('.add-item-btn').on('click', function(){
+    	addItem();
+    })
+
 });
+
+
+var addItem = function(){
+	var newItem = $('input.add-new-item').val();
+    $('.list-items').prepend('<li>'+ newItem +'</li>');
+}
 
 // end click me
 
-// begin click to complete 
-
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if( ev.target.tagName === 'LI') {
-     ev.target.classList.toggle('done'); 
-  }
-}, false);
-
-// end click to complete
